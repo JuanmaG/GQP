68d264398d8793f88a9941de6fae1ad536591389
Juanma <jgarridolechuga@gmail.com>
2018-04-05 18:38:35 +0200

    borrador

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,16 +2,17 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { PERROS } from "../../data/data.perros";
 import { Perros } from "../../interfaces/perros.interface";
+import {VistaPage} from "../index.paginas";
   @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
     perros:Perros[] = [];
-  constructor() {
+  constructor(public navCtrl:NavController) {
     this.perros = PERROS.slice(0);
   }
-  vista(  perro:Perros  ){
-    console.log( perro );
+  vista(){
+    this.navCtrl.push(VistaPage);
   }
 }
---
+++ src/pages/index.paginas.ts
@@ -0,0 +1,1 @@
+export{VistaPage}from "./vista/vista";
