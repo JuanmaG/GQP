aabaeff982a9db662741f827a76e4ccb70f8a641
Juanma <jgarridolechuga@gmail.com>
2018-04-10 13:13:13 +0200

    github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class MyApp {
   rootPage:any = LoginPage;
   browser=HomePage;
   anadir=AnadirPage;
+  login=LoginPage;
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ImagePicker,ImagePickerOptions} from '@ionic-native/image-picker';
+import { HomePage } from '../home/home';
+import { ToastController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -9,13 +11,30 @@ import { ImagePicker,ImagePickerOptions} from '@ionic-native/image-picker';
   templateUrl: 'login.html',
 })
 export class LoginPage {
-
+  browser=HomePage;
   slideOneForm: FormGroup;
-  constructor(public navCtrl: NavController, public navParams: NavParams,public formBuilder: FormBuilder,private imagePicker: ImagePicker) {
+  constructor(private toastCtrl: ToastController,public navCtrl: NavController, public navParams: NavParams,public formBuilder: FormBuilder,private imagePicker: ImagePicker) {
     this.slideOneForm = formBuilder.group({
     firstName: [''],
     lastName: [''],
     age: ['']
 });
+}
+
+presentToast() {
+  let toast = this.toastCtrl.create({
+    message: 'Work In Progress',
+    duration: 3000,
+    position: 'top'
+  });
+
+  toast.onDidDismiss(() => {
+    console.log('Dismissed toast');
+  });
+
+  toast.present();
+}
+abrirPagina(pagina:any){
+  this.navCtrl.push(pagina);
 }
 }
