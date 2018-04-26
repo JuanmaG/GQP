b186b0575c877f3d57e896125c72411e7e0245d7
Juanma <jgarridolechuga@gmail.com>
2018-04-26 09:46:42 +0200

    Working on facebook login,added logout and a display of info to check if the user has logged in correctly,crashes if logs out and tryes to relog

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -15,60 +15,78 @@ export class LoginPage {
   slideOneForm: FormGroup;
   isLoggedIn:boolean = false;
   users: any;
-  constructor(private fb: Facebook,private toastCtrl: ToastController,public navCtrl: NavController, public navParams: NavParams,public formBuilder: FormBuilder,private imagePicker: ImagePicker) {
-    this.slideOneForm = formBuilder.group({
-    firstName: [''],
-    lastName: [''],
-    age: ['']
-});
-fb.getLoginStatus()
-  .then(res => {
-    console.log(res.status);
-    if(res.status === "connect") {
-      this.isLoggedIn = true;
-    } else {
-      this.isLoggedIn = false;
-    }
-  })
-  .catch(e => console.log(e));
-}
-
-presentToast() {
-  let toast = this.toastCtrl.create({
-    message: 'Work In Progress',
-    duration: 3000,
-    position: 'top'
+  constructor(private fb: Facebook,
+              private toastCtrl: ToastController,
+              public navCtrl: NavController,
+              public navParams: NavParams,
+              public formBuilder: FormBuilder,
+              private imagePicker: ImagePicker) {
+      this.slideOneForm = formBuilder.group({
+      firstName: [''],
+      lastName:[''],
+      age: ['']
   });
-
-  toast.onDidDismiss(() => {
-    console.log('Dismissed toast');
-  });
-
-  toast.present();
-}
-abrirPagina(pagina:any){
-  this.navCtrl.push(pagina);
-}
-login() {
-  this.fb.login(['public_profile', 'user_friends', 'email'])
+  //Para comprobar si se esta logeado
+  fb.getLoginStatus()
     .then(res => {
-      if(res.status === "connected") {
+      console.log(res.status);
+      if(res.status === "connect") {
         this.isLoggedIn = true;
-        this.getUserDetail(res.authResponse.userID);
       } else {
         this.isLoggedIn = false;
       }
     })
-    .catch(e => console.log('Error logging into Facebook', e));
-}
-getUserDetail(userid) {
-  this.fb.api("/"+userid+"/?fields=id,email,name,picture,gender",["public_profile"])
-    .then(res => {
-      console.log(res);
-      this.users = res;
-    })
-    .catch(e => {
-      console.log(e);
+    .catch(e => console.log(e));
+  }
+
+  presentToast() {
+    let toast = this.toastCtrl.create({
+      message: 'Work In Progress',
+      duration: 3000,
+      position: 'top'
     });
-}
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
+  }
+  //Funcion para abrir pagina
+  abrirPagina(pagina:any){
+    this.navCtrl.push(pagina);
+  }
+
+  //Funcion de logout para el plugin de facebook
+  logout() {
+    this.fb.logout()
+      .then( res => this.isLoggedIn = false)
+      .catch(e => console.log('Error logout from Facebook', e));
+  }
+
+  //Funcion de login con el plugin de facebook
+  login() {
+    this.fb.login(['public_profile', 'user_friends', 'email'])
+      .then(res => {
+        if(res.status === "connected") {
+          this.isLoggedIn = true;
+          this.getUserDetail(res.authResponse.userID);
+        } else {
+          this.isLoggedIn = false;
+        }
+      })
+      .catch(e => console.log('Error logging into Facebook', e));
+  }
+
+  //Funcion para conseguir datos del usuario logado
+  getUserDetail(userid) {
+    this.fb.api("/"+userid+"/?fields=id,email,name,picture,gender",["public_profile"])
+      .then(res => {
+        console.log(res);
+        this.users = res;
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
 }
--- src/pages/misanuncios/misanuncios.ts
+++ src/pages/misanuncios/misanuncios.ts
@@ -9,35 +9,43 @@ import { Screenshot } from '@ionic-native/screenshot';
 })
 export class MisanunciosPage {
 
-  constructor(private screenshot: Screenshot,public socialSharing: SocialSharing,public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private screenshot: Screenshot,
+              public socialSharing: SocialSharing,
+              public navCtrl: NavController,
+              public navParams: NavParams) {
   }
-  share(img){
-    var msg = "compartidoViaGuauQueAnimales";
-    let imagen="data:image/jpeg;base64," + img;
-    this.screenshot.URI(80)
-      .then((res) => {
-        this.socialSharing.share(msg,null, res.URI, null)
-         .then(() => {},
-           () => {
-             alert('SocialSharing failed');
-           });
-         },
-        () => {
-        alert('Screenshot failed');
-        });
-      }
 
-    facebookShare() {
-    this.screenshot.URI(80)
-      .then((res) => {
-        this.socialSharing.shareViaFacebook(null, res.URI, null)
-         .then(() => {},
-           () => {
-             alert('SocialSharing failed');
-           });
-         },
-        () => {
-        alert('Screenshot failed');
-        });
-      }
+  //Funcion que usa SocialSharing que toma un screenshot de la pantalla
+  //y manda un mensaje + la foto mediante la app que elija el usuario,si es posible
+share(img){
+  var msg = "compartidoViaGuauQueAnimales";
+  let imagen="data:image/jpeg;base64," + img;
+  this.screenshot.URI(80)
+    .then((res) => {
+      this.socialSharing.share(msg,null, res.URI, null)
+        .then(() => {},
+          () => {
+            alert('SocialSharing failed');
+         });
+       },
+      () => {
+      alert('Screenshot failed');
+      });
+  }
+
+//Funcion que comparte mediante facebook,haciendo un screenshot de la pantalla y compartiendo esa imagen y
+//un mensaje,si se esta loggado en FB
+facebookShare() {
+  this.screenshot.URI(80)
+    .then((res) => {
+      this.socialSharing.shareViaFacebook(null, res.URI, null)
+        .then(() => {},
+         () => {
+           alert('SocialSharing failed');
+         });
+       },
+      () => {
+      alert('Screenshot failed');
+      });
+    }
 }
